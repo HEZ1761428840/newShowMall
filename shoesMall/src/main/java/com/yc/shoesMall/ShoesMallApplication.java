c4b4c1a90d8f06b975cc2bdd7590dedfc3f54ccd
HEZ1761428840 <1761428840@qq.com>
2019-09-08 14:33:09 +0800

    监听器

--- shoesMall/src/main/java/com/yc/shoesMall/ShoesMallApplication.java
+++ shoesMall/src/main/java/com/yc/shoesMall/ShoesMallApplication.java
@@ -1,21 +1,35 @@
 package com.yc.shoesMall;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.yc.shoesMall.web.LoginInterceptor;
+
 
 @SpringBootApplication
 @Controller
 @MapperScan("com.yc")
-public class ShoesMallApplication {
+public class ShoesMallApplication implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShoesMallApplication.class, args);
 
+	}
+	
+	/**
+	 * 注册拦截器， 当容器启动时， 该方法会被调用，那么注册拦截器就会被加载导容器中
+	 */
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		
+		registry.addInterceptor(new LoginInterceptor())
+		.addPathPatterns("/toedit");
+		
 	}
 
 }
